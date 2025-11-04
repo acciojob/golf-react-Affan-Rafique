0459fb19b00f36e822e2febbc535c9e3b0677355
Affan-Rafique <affanrafique9@gmail.com>
2025-11-04 16:03:59 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+// Webpack 4 template uses ReactDOM.render
+ReactDOM.render(<App />, document.getElementById("root"));
 
